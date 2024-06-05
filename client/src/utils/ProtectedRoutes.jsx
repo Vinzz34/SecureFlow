845d95973d8ac97e6fa3a56656efb7cbe341bf29
vinzz34 <vinaygn29@gmail.com>
2024-06-05 21:49:53 +0530

    removed protected routes component

--- client/src/utils/ProtectedRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom"
-
-const ProtectedRoutes = () => {
-
-
-  return currentUser === null ? <Navigate to='/sign-in' replace={true} />  : <Outlet />
-}
-
-export default ProtectedRoutes
